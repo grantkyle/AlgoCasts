64bc2183af12cfab7ad4121668579ae5db09e3ad
Grant Kyle <sgrantkyle@gmail.com>
2021-03-29 14:18:49 -0500

    reverse integer multiple solutions

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,23 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n.toString().split("").reverse().join("");
+
+  return parseInt(reversed) * Math.sign(n);
+}
+
+// but also can use any of the 'reverse string' methods to produce the same result
+
+// function reverseInt(n) {
+//   const reversed = n
+//     .toString()
+//     .split("")
+//     .reduce((character, reversed) => {
+//       return reversed + character;
+//     });
+
+//   return parseInt(reversed) * Math.sign(n);
+// }
 
 module.exports = reverseInt;
