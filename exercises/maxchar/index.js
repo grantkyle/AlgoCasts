c79502f15ccead408f5ecaa269b0a1eba8cca4a3
Grant Kyle <sgrantkyle@gmail.com>
2021-03-29 15:43:41 -0500

    explanation of variations of max characters question, maxChar question answered

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// common variations of this question
+// what is the most common character in the string?
+// does string A have the same characters as string B (is it an anagram)?
+// does the given string have any repeated characters in it?
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+      if (charMap[char] > max) {
+          max = charMap[char];
+          maxChar = char;
+      }
+
+      return maxChar;
+  }
+
+  console.log(charMap)
+}
 
 module.exports = maxChar;
